4b809e0d54281aceb462b44303746eb164d713aa
Bala Naren Chanumolu <4568naren@gmail.com>
2023-11-26 18:52:25 -0500

    frontend initial code

--- pa3_frontend/src/app/app.routes.ts
+++ pa3_frontend/src/app/app.routes.ts
@@ -1,3 +1,14 @@
-import { Routes } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { HomePageComponent } from './home-page/home-page.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: HomePageComponent },
+    // ... other routes
+];
+
+@NgModule({
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
+})
+export class AppRoutingModule { }

38a522dc73a9860e206e0a38a2ca16bb5f76fcb8
Bala Naren Chanumolu <4568naren@gmail.com>
2023-11-26 18:31:58 -0500

    added component to frontend

---
+++ pa3_frontend/src/app/survey-list/survey-list.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-survey-list',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './survey-list.component.html',
+  styleUrl: './survey-list.component.css'
+})
+export class SurveyListComponent {
+
+}
